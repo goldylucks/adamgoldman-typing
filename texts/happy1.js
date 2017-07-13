ff8107ed3f40d7c4734ab5cdd0e5681e17cd2b51
goldylucks <adamgoldman7@gmail.com>
2017-07-13 17:47:23 +0800

    add common words to happy1 text

--- texts/happy1.js
+++ texts/happy1.js
@@ -1,4 +1,5 @@
 export default {
   title: 'happy 1',
+  words: ['happy', 'things', 'are', 'that', 'feel'],
   body: 'Happy things are things that are just things that are feeling happy and it\'s easy to feel happy as happy things are just happy in a way that feels good now.',
 }
