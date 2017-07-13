de4136d1aa579fe8b88b7be791a11bcc63ac92a2
goldylucks <adamgoldman7@gmail.com>
2017-07-13 17:46:41 +0800

    add text

---
+++ texts/happy3.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'its good to be good',
+  words: ['good', 'the', 'be', 'to', 'of', 'and', 'a', ''],
+  body: 'it is good to be as good to others as you can be good to many things and too good for others as a type of good feeling and being to another now.',
+}
